307f7e74aeddced2ba46a4e7afa6b4f622af243f
akashg524 <akashg524@gmail.com>
2018-09-12 15:19:40 +0530

    done

--- Transform.Tests/TransformTest.cs
+++ Transform.Tests/TransformTest.cs
@@ -12,8 +12,8 @@ namespace Transform.Tests
         public void ShouldSquareTheNumbers()
         {
             int[] numbers = new int[] { 1, 2, 3, 5 };
-            int[] doubled = numbers.Map(x => x * 2).ToArray();
-            int[] expected = new int[] { 2, 4, 6, 10 };
+            int[] doubled = numbers.Map(x => x * x).ToArray();
+            int[] expected = new int[] { 1, 4, 9, 25 };
             Assert.Equal(doubled, expected);
         }
     }
--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -4,11 +4,16 @@ using System.Collections.Generic;
 
 namespace Transform
 {
+    public delegate  int mydel(int x);
     public static class Transform 
     {
-        public static void Map()
+        public static int[] Map(this int[] ar,mydel mydel1 )
         {
-            throw new NotImplementedException();
+           for(int i=0;i<ar.Length;i++)
+           {
+               ar[i]=mydel1(ar[i]);
+           }
+           return ar;
         }
     }
 }
